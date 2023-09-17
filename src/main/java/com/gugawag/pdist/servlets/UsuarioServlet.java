14494c33c35afc6633000fbd87bbe1f3ab712e4f
henrqueDev <lhdeveloper137@gmail.com>
2023-09-17 02:45:36 -0300

    commit 2 - correção

--- src/main/java/com/gugawag/pdist/servlets/UsuarioServlet.java
+++ src/main/java/com/gugawag/pdist/servlets/UsuarioServlet.java
@@ -1,6 +1,8 @@
 package com.gugawag.pdist.servlets;
 
+import com.gugawag.pdist.ejb.session.MensagemService;
 import com.gugawag.pdist.ejb.session.UsuarioService;
+import com.gugawag.pdist.modelo.Mensagem;
 import com.gugawag.pdist.modelo.Usuario;
 import jakarta.ejb.EJB;
 import jakarta.servlet.annotation.WebServlet;
@@ -16,6 +18,8 @@ public class UsuarioServlet extends HttpServlet {
 
     @EJB(lookup="java:module/usuarioService")
     private UsuarioService usuarioService;
+    @EJB(lookup="java:module/mensagemService")
+    private MensagemService mensagemService;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String operacao = request.getParameter("oper");
@@ -24,11 +28,24 @@ public class UsuarioServlet extends HttpServlet {
             case "1": { // inserir
                 long id = Integer.parseInt(request.getParameter("id"));
                 String nome = request.getParameter("nome");
+                if(id == 4){
+                    usuarioService.inserir(id, nome);
+                    this.mensagemService.inserir(id, "mensagem");
+                }
+                if(id == 5){
+                    usuarioService.inserir(id, nome);
+                    this.mensagemService.inserir(id, "mensagem");
+                    throw new RuntimeException();
+                }
                 usuarioService.inserir(id, nome);
             }
             case "2": { // listar
                 for(Usuario usuario: usuarioService.listar()){
                     resposta.println(usuario.getNome());
+                }
+                resposta.println("\n\n -----MENSAGENS ABAIXO----\n\n");
+                for(Mensagem msg: this.mensagemService.listar()){
+                    resposta.println(msg.getConteudo());
                 }
                 break;
             }
